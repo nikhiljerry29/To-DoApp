80625bc354001b170e5b01ab48fdba63ea0ba34a
NIKHIL GUPTA <nikhil.gupta5667@gmail.com>
2021-01-17 16:57:10 +0530

    Basic UI with UI and Template updated

--- README.md
+++ README.md
@@ -3,8 +3,13 @@ Basic Productivity Application for daily use.
 
 ## Features To Be Updated
 1. Database integration
+	1. Creation of task details basically details. (Area of discussion)
 2. Credential Access
 3. Delete Button
+4. Customizable Category
+
+## Defects to be fixed
+1. Empty list handling and redirecting to proper location
 
 ## Public API Used
 1. [Kyoz-iquotes](https://github.com/kyoz/iquotes#readme)
--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const iquotes = require('iquotes');
+const date = require(__dirname + "/date.js");
+
+const displayDate = date.getDate();
 
 const app = express();
 
-var items = ["Make Bed", "Take Dog for a walk", "Buy Grocery"];
+const items = ["Make Bed", "Take Dog for a walk", "Buy Grocery"];
+const workItems = []
 
 app.set("view engine", "ejs");
 
@@ -12,17 +16,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-	var today = new Date();
-	
-	var options = {
-		weekday: "long",
-		day: "numeric",
-		month: "long"
-	};
-
-	var displayDate = today.toLocaleDateString("en-US", options);
-
 	res.render("list", {
+		listCategory : "Personal",
 		displayDate : displayDate,
 		newListItem : items,
 		quotesFull : iquotes.random('life')
@@ -30,13 +25,30 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res) {
-	item = req.body.newItem;
-	if (item !== "")
-		items.push(item)
-	res.redirect("/");
+	const item = req.body.newItem;
+	if (item !== ""){
+		if(req.body.list === "Work") {
+			workItems.push(item);
+			res.redirect("/work");
+		}else {
+			items.push(item);
+			res.redirect("/");
+		}
+	}
+
 });
 
-let port = 8080;
+app.get("/work", function(req, res) {
+	res.render("list", {
+		listCategory : "Work",
+		displayDate : displayDate,
+		newListItem : workItems,
+		quotesFull : iquotes.random('life')
+	});
+
+});
+
+const port = 8080;
 app.listen(port, function() {
 	console.log('Server started on port :: ' + port);
 });
